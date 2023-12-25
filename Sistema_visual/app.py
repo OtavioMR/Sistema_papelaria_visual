7b27c3ca267b602eafd8e7f8a07121edfa3a7738
Otavio Monteiro <otavvioo11@gmail.com>
2023-12-25 13:14:47 -0300

    Primeiros arquivos

---
+++ Sistema_visual/app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+menu_inicial = Tk()
+menu_inicial.title('Papelaria Otávio')
+
+menu_inicial.geometry('500x250+200+200')
+menu_inicial.resizable(True,True)
+menu_inicial.iconbitmap('Sistema_visual\Imagens\_123034.ico')
+
+menu_inicial.mainloop()
